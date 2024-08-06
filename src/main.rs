aa09e32d334c594a1d0775e3ce2f104217dfcb20
JuanMarceloT <jmarcelot2013@gmail.com>
2024-08-06 10:40:18 -0300

    Add Star struct and read_csv with generic types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+use serde::de::DeserializeOwned;
+use serde::{Deserialize};
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File};
+use std::io::{BufReader};
 use std::path::Path;
 use std::time::{Duration, Instant};
-use std::io::{BufWriter, Write};
-use csv::StringRecord;
 
-#[derive(Debug, Clone)]
+
+
+#[derive(Debug, Clone, Deserialize)]
+#[allow(dead_code)]
 struct Player {
-    id: i32,
+    sofifa_id: u32,
     short_name: String,
     long_name: String,
-    player_positions: Vec<String>,
+    player_positions: String, 
     nationality: String,
     club_name: String,
     league_name: String,
 }
+
+#[derive(Debug, Clone, Deserialize)]
+#[allow(dead_code)]
+struct Star {
+    user_id: i32,
+    sofifa_id: u32,
+    rating: f32,
+}
+
 
 trait Identifiable {
     fn id(&self) -> i32;
@@ -22,24 +35,56 @@ trait Identifiable {
 
 impl Identifiable for Player {
     fn id(&self) -> i32 {
-        self.id
+        self.sofifa_id as i32
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+impl Identifiable for Star {
+    fn id(&self) -> i32 {
+        self.sofifa_id as i32
+    }
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn main() {
+
+    let modulo   = 2000;
+    let mut hash_table: Vec<Vec<Star>> = initialize_hash_table(modulo);
+
+
+    let x = read_csv("rating.csv", |record| {
+        //println!("{:?}", record);
+        insert(record, modulo, &mut hash_table);
+    });
+
+    let player = vec![247202, 254155, 257697, 257697, 158023, 257727, 186941, 228101, 243758, 203458];
+    
+
+    player.iter().for_each(|x| {
+        match search(*x, modulo, &hash_table) {
+            Some(player) => {
+                println!("{:?}", player.rating);
+            },
+            None => {
+                // Optional: Uncomment the line below to print messages for not found players
+                // println!("{:?} NÃO ENCONTRADO", x);
+            }
+        }
+    });
+    
+
+}
+
+fn initialize_hash_table<T: Clone>(modulo: usize) -> Vec<Vec<T>> {
+    vec![Vec::new(); modulo]
+}
 
 fn insert<T: Identifiable>(item: T, modulo: usize, hash_table: &mut [Vec<T>]) {
     let index = (item.id() as usize) % modulo;
     hash_table[index].push(item);
 }
-fn search<T: Identifiable + Clone>(
-    id: i32,
-    modulo: usize,
-    hash_table: &[Vec<T>],
-) -> Option<T> {
+fn search<T: Identifiable + Clone>(id: i32, modulo: usize, hash_table: &[Vec<T>]) -> Option<T> {
     let index = (id as usize) % modulo;
 
     for item in &hash_table[index] {
@@ -50,22 +95,26 @@ fn search<T: Identifiable + Clone>(
     None
 }
 
-fn read_csv<P, F>(filename: P, mut func: F) -> Result<(), Box<dyn Error>>
+fn read_csv<P, F, T>(filename: P, mut func: F) -> Result<(), Box<dyn Error>>
 where
     P: AsRef<Path>,
-    F: FnMut(StringRecord),
+    F: FnMut(T),
+    T: DeserializeOwned,
 {
     let file = File::open(filename)?;
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
+    let reader = BufReader::new(file);
+
+    let mut rdr = csv::Reader::from_reader(reader);
+
+    for result in rdr.deserialize() {
+        let record: T = result?;
         func(record);
     }
     Ok(())
 }
 
 
-
+#[allow(dead_code)]
 fn measure_time<F>(func: F) -> Duration
 where
     F: FnOnce(),
