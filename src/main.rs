630f5349db0089837f894c4a718de31ee91be6cf
JuanMarceloT <jmarcelot2013@gmail.com>
2024-08-15 12:07:39 -0300

    Handle 'user' command in parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use prettytable::{Cell, Row, Table};
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
+use std::cmp::Ordering;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::{stdin, stdout, Write};
 use std::path::Path;
 use std::time::{Duration, Instant};
-use std::cmp::Ordering;
-use prettytable::{Table, Row, Cell};
-use std::io::{stdin,stdout,Write};
 
 mod hash_table;
 mod trie;
@@ -32,7 +32,6 @@ struct RatingFile {
     rating: f32,
 }
 
-
 #[derive(Debug, Clone, Deserialize)]
 #[allow(dead_code)]
 struct RatingPlayer {
@@ -49,15 +48,12 @@ struct Tags {
     tag: String,
 }
 
-
-
 #[derive(Debug, Clone, Deserialize)]
 #[allow(dead_code)]
 struct User {
     user_id: u32,
-    ratings: Vec<RatingPlayer>
+    ratings: Vec<RatingPlayer>,
 }
-
 
 trait Identifiable {
     fn id(&self) -> u32;
@@ -87,8 +83,6 @@ impl Identifiable for RatingPlayer {
     }
 }
 
-
-
 trait AddRating<T> {
     fn add_rating(&mut self, rating: T);
 }
@@ -106,11 +100,12 @@ impl AddRating<RatingPlayer> for User {
     }
 }
 
-
-
-
-fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_table::HashMap<u32, Player>, rating : &mut hash_table::HashMap<u32, RatingPlayer>) -> Option<Vec<Player>>{
-
+fn get_player_start_with(
+    prefix: &str,
+    trie: &mut trie::Trie,
+    play: &mut hash_table::HashMap<u32, Player>,
+    rating: &mut hash_table::HashMap<u32, RatingPlayer>,
+) -> Option<Vec<Player>> {
     let mut result = Vec::<Player>::new();
 
     let mut ratings = Vec::<RatingPlayer>::new();
@@ -120,16 +115,15 @@ fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_
         match trie.get_id(&name) {
             Some(n) => {
                 for i in n {
-
                     match rating.search(&i) {
-                        Some(rate ) =>{
+                        Some(rate) => {
                             // println!("{:?} {:?} {:?} ", (rate.rating_sum/(rate.num_ratings as f32)), rate.num_ratings as f32, rate.rating_sum);
                             ratings.push(rate.clone());
-                        },
+                        }
                         None => {}
                     }
                 }
-            },
+            }
             None => {
                 println!("No match found for {}", name);
             }
@@ -140,7 +134,7 @@ fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_
         let avg_a = if a.num_ratings > 0 {
             a.rating_sum / a.num_ratings as f32
         } else {
-            0.0  // for players with no ratings
+            0.0 // for players with no ratings
         };
 
         let avg_b = if b.num_ratings > 0 {
@@ -166,12 +160,12 @@ fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_
 
     for rating in ratings {
         if let Some(player) = play.search(&rating.sofifa_id) {
-             result.push(player.clone());
+            result.push(player.clone());
 
             let mut stars = 0.0;
 
             if rating.num_ratings > 0 {
-                stars = rating.rating_sum/(rating.num_ratings as f32);
+                stars = rating.rating_sum / (rating.num_ratings as f32);
             }
             table.add_row(Row::new(vec![
                 Cell::new(&player.sofifa_id.to_string()),
@@ -182,9 +176,8 @@ fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_
                 Cell::new(&rating.num_ratings.to_string()),
             ]));
 
-             //println!("{:?} {:?} {:?} {:?} {:?} {:?}", player.sofifa_id, player.short_name, player.long_name, player.player_positions, format!("{:.6}", stars), rating.num_ratings);
-
-         }
+            //println!("{:?} {:?} {:?} {:?} {:?} {:?}", player.sofifa_id, player.short_name, player.long_name, player.player_positions, format!("{:.6}", stars), rating.num_ratings);
+        }
     }
 
     table.printstd();
@@ -195,9 +188,8 @@ fn get_player_start_with(prefix: &str, trie: &mut trie::Trie, play : &mut  hash_
 #[allow(dead_code)]
 #[allow(unused_variables)]
 fn main() {
-
     let start = Instant::now();
-    
+
     let modulo = 300000;
     let mut rating_table: hash_table::HashMap<u32, RatingPlayer> = hash_table::HashMap::new(3_000);
     let mut players_table: hash_table::HashMap<u32, Player> = hash_table::HashMap::new(3_000);
@@ -206,12 +198,11 @@ fn main() {
     let mut name_index = trie::Trie::new();
     let mut tag_player = trie::Trie::new();
 
-
     let x = read_csv("players.csv", |record: Player| {
         //println!("{:?}", record);
         players_table.insert(record.id(), record.clone());
 
-        let temp = RatingPlayer{
+        let temp = RatingPlayer {
             sofifa_id: record.sofifa_id,
             rating_sum: 0.0,
             num_ratings: 0,
@@ -221,21 +212,21 @@ fn main() {
         name_index.insert_with_id(&record.long_name, record.id());
     });
 
-    let x = read_csv("minirating.csv", |record: RatingFile| {
+    let x = read_csv("rating.csv", |record: RatingFile| {
         //println!("{:?}", record);
         match user_table.search(&record.user_id) {
             Some(user) => {
-                user.ratings.push(RatingPlayer{
+                user.ratings.push(RatingPlayer {
                     sofifa_id: record.sofifa_id,
                     rating_sum: record.rating,
                     num_ratings: 1,
                 });
-               // println!("{:?}\n\n", user)
+                // println!("{:?}\n\n", user)
             }
             None => {
                 let user = User {
                     user_id: record.user_id,
-                    ratings: vec![RatingPlayer{
+                    ratings: vec![RatingPlayer {
                         sofifa_id: record.sofifa_id,
                         rating_sum: record.rating,
                         num_ratings: 1,
@@ -250,73 +241,164 @@ fn main() {
         rating.as_mut().unwrap().add_rating(record.rating);
         //println!("{:?}", rating);
     });
-    
+
     let x = read_csv("tags.csv", |record: Tags| {
         //println!("{:?}", record.tag);
         tag_player.insert_with_id(&record.tag, record.sofifa_id);
     });
 
-
     // get_player_start_with("fer", &mut name_index, &mut players_table, &mut rating_table);
-    
 
     let duration = start.elapsed();
     println!("Tempo gasto: {:?}", duration);
     println!("player table {:?}", players_table.occupancy());
     println!("rating table {:?}", rating_table.occupancy());
     println!("user table {:?}", user_table.occupancy());
-    println!("player table avg {:?}", players_table.average_bucket_length());
-    println!("rating table avg {:?}", rating_table.average_bucket_length());
+    println!(
+        "player table avg {:?}",
+        players_table.average_bucket_length()
+    );
+    println!(
+        "rating table avg {:?}",
+        rating_table.average_bucket_length()
+    );
     println!("user table avg {:?}", user_table.average_bucket_length());
 
-    let mut s=String::new();
+    let mut s = String::new();
 
     while s != "n" {
         s.clear();
         print!("> ");
-        let _=stdout().flush();
-        stdin().read_line(&mut s).expect("Did not enter a correct string");
-        if let Some('\n')=s.chars().next_back() {
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut s)
+            .expect("Did not enter a correct string");
+        if let Some('\n') = s.chars().next_back() {
             s.pop();
         }
-        if let Some('\r')=s.chars().next_back() {
+        if let Some('\r') = s.chars().next_back() {
             s.pop();
         }
         let words: Vec<&str> = s.split_whitespace().collect();
 
         if words.len() < 2 {
             println!("Insufficient arguments");
-        }else {
-            match (words[0].to_lowercase()).as_str()
-            {
+        } else {
+            match (words[0].to_lowercase()).as_str() {
                 "player" => {
                     let arg = remove_outer_quotes(&words[1..words.len()].join(" "));
                     // println!("{}", arg);
-                    get_player_start_with(&arg, &mut name_index, &mut players_table, &mut rating_table);
-                    },
+                    get_player_start_with(
+                        &arg,
+                        &mut name_index,
+                        &mut players_table,
+                        &mut rating_table,
+                    );
+                }
                 "user" => {
-                    println!("{}", words[1]);
-                },
+                    let id = remove_outer_quotes(words[1]).parse::<u32>();
+
+                    if id.is_err() {
+                        println!("Invalid user id");
+                    }
+
+                    if id.is_ok() {
+                        // println!("{}", id.unwrap());
+
+                        let user_rating = &mut user_table.search(&id.unwrap()).unwrap().ratings;
+
+                        user_rating.sort_by(|a, b| {
+                            let avg_a = if a.num_ratings > 0 {
+                                a.rating_sum / a.num_ratings as f32
+                            } else {
+                                0.0 // for players with no ratings
+                            };
+
+                            let avg_b = if b.num_ratings > 0 {
+                                b.rating_sum / b.num_ratings as f32
+                            } else {
+                                0.0
+                            };
+
+                            avg_b.partial_cmp(&avg_a).unwrap_or(Ordering::Equal)
+                        });
+
+                        //*user_rating = user_rating[0..19].to_vec();
+
+                        user_rating.sort_by(|a, b| {
+
+                            let global_rating_info_a =
+                                    rating_table.search(&a.sofifa_id).unwrap().clone();
+
+                            let global_rating_info_b =
+                                    rating_table.search(&b.sofifa_id).unwrap().clone();
+
+                            let user_rating_a = a.rating_sum / (a.num_ratings as f32); 
+                            
+                            let user_rating_b = b.rating_sum / (b.num_ratings as f32); 
+
+
+                            let avg_a = if global_rating_info_a.num_ratings > 0 {
+                                (global_rating_info_a.rating_sum / global_rating_info_a.num_ratings as f32) + user_rating_a * 10.0
+                            } else {
+                                0.0 // for players with no ratings
+                            };
+
+                            let avg_b = if global_rating_info_b.num_ratings > 0 {
+                                (global_rating_info_b.rating_sum / global_rating_info_b.num_ratings as f32) + user_rating_b * 10.0
+                            } else {
+                                0.0
+                            };
+
+                            avg_b.partial_cmp(&avg_a).unwrap_or(Ordering::Equal)
+                        });
+
+                        let mut index = 0;
+
+                        for player in user_rating {
+                            if index < 20 {
+                                let player_infos = players_table.search(&player.sofifa_id).unwrap();
+
+                                let rating = player.rating_sum / player.num_ratings as f32;
+
+                                let global_rating_info =
+                                    rating_table.search(&player.sofifa_id).unwrap();
+
+                                let global_rating = global_rating_info.rating_sum
+                                    / global_rating_info.num_ratings as f32;
+
+                                println!(
+                                    "{:?} {:?} {:?} {:?} {:?} {:?}",
+                                    player_infos.sofifa_id,
+                                    player_infos.short_name,
+                                    player_infos.long_name,
+                                    global_rating,
+                                    global_rating_info.num_ratings,
+                                    rating
+                                );
+                                index+=1;
+                            }
+                        }
+                    }
+                }
                 "tags" => {
-                    for tag in 1..words.len(){
+                    for tag in 1..words.len() {
                         println!("{}", remove_outer_quotes(words[tag]));
                     }
-                },
+                }
                 _ if words[0].to_lowercase().starts_with("top") => {
                     let number_part = &words[0][3..];
                     match number_part.parse::<u32>() {
                         Ok(number) => {
                             println!("Top number: {}", number)
-                        },
+                        }
                         Err(_) => println!("Invalid top number"),
                     }
                     println!("{}", remove_outer_quotes(words[1]));
-                },
+                }
                 _ => println!("Invalid"),
-        
             }
         }
-
     }
 }
 
@@ -350,8 +432,6 @@ where
     duration
 }
 
-
 fn remove_outer_quotes(s: &str) -> String {
     s.trim_matches(|c| c == '"' || c == '\'').to_string()
 }
-
